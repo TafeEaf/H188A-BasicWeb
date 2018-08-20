01f04de73dd0e2da8831831d45eedce7ec31052a
TafeEaf <39146089+TafeEaf@users.noreply.github.com>
2018-08-20 16:56:21 +1000

    Update code

--- ESP8266-BasicServer.ino
+++ ESP8266-BasicServer.ino
@@ -1,79 +1,92 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
- 
-#define ssid      "Classroom"   // WiFi SSID
-#define password  "12345678"    // WiFi password
-#define LEDPIN    D3            // Led Pin
 
-String  LedStatus = "OFF";
- 
+#define ssid      "WemosWifi"     // WiFi SSID
+#define password  "12345678"      // WiFi password
+#define GreenLed    D2            // Led Pin
+
+#define  GreenLedOn   "<font color=\"#7fbf7f\"><b>ON</b></font>"
+#define  GreenLedOff  "<font color=\"#006600\"><b>OFF</b></font>"
+
+String GreenLedStatus;
+
 // create Objects
 ESP8266WebServer server ( 80 );
- 
+
 String getPage(){
-  String page = "<html lang=fr-FR><head><meta http-equiv='refresh' content='10'/>";
-  page += "<title>ESP8266 Demo</title>";
+  String page = "<html lang=en-EN><head><meta http-equiv='refresh' content='10'/>";
+  page += "<title>ESP8266</title>";
   page += "<style> body { background-color: #fffff; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }</style>";
-  page += "</head><body><h1>ESP8266 Demo</h1>";
-  page += "<h3>GPIO</h3>";
+  page += "</head><body><h1>Wemos Wifi Basic Server</h1>";
+  page += "<h3>Remote GPIO configuration</h3>";
   page += "<form action='/' method='POST'>";
-  page += "<ul><li>D3 (Current status: ";
-  page += LedStatus;
+  
+  // handle Green LED
+  page += "<ul><li>Green Led (Current status: ";
+  page += GreenLedStatus;
   page += ")";
-  page += "<INPUT type='radio' name='LED' value='1'>ON";
-  page += "<INPUT type='radio' name='LED' value='0'>OFF</li></ul>";
-  page += "<INPUT type='submit' value='Actualiser'>";
+  page += "<INPUT type='radio' name='LED1' value='0'>ON";
+  page += "<INPUT type='radio' name='LED1' value='1'>OFF</li></ul>";
+
+  // send form
+  page += "<INPUT type='submit' value='Send'>";
   page += "</body></html>";
   return page;
 }
 
-void handleRoot(){ 
-  if ( server.hasArg("LED") ) {
+
+void handleSubmit() {
+  String LED_1_Value = server.arg("LED1");
+
+  // update Green LED
+  Serial.print("Set Green Led: ");
+  if ( LED_1_Value == "0" ) {
+    digitalWrite(GreenLed, 0);
+    GreenLedStatus = GreenLedOn;
+  } else if ( LED_1_Value == "1" ) {
+    digitalWrite(GreenLed, 1);
+    GreenLedStatus = GreenLedOff;
+  } else {
+    Serial.print("No parameter sent, using previous: ");
+  }
+  Serial.println(!digitalRead(GreenLed));
+
+  server.send ( 200, "text/html", getPage() );
+}
+
+void handleRoot(){
+  if ( server.hasArg("LED1")) {
     handleSubmit();
   } else {
     server.send ( 200, "text/html", getPage() );
-  }  
-}
- 
-void handleSubmit() {
-  // Update GPIO 
-  String LEDValue;
-  LEDValue = server.arg("LED");
-  Serial.println("Set GPIO "); Serial.print(LEDValue);
-  if ( LEDValue == "1" ) {
-    digitalWrite(LEDPIN, 1);
-    LedStatus = "On";
-    server.send ( 200, "text/html", getPage() );
-  } else if ( LEDValue == "0" ) {
-    digitalWrite(LEDPIN, 0);
-    LedStatus = "Off";
-    server.send ( 200, "text/html", getPage() );
-  } else {
-    Serial.println("Err Led Value");
   }
 }
- 
+
 void setup() {
+  pinMode(GreenLed, OUTPUT);            // sets the digital pin D2 as output
+  digitalWrite(GreenLed, 1);            // sets the digital pin D2 high
+  GreenLedStatus  =   GreenLedOff;
+
   Serial.begin ( 115200 );
 
   WiFi.begin ( ssid, password );
-  // Attente de la connexion au réseau WiFi / Wait for connection
+  // Wait for connection
   while ( WiFi.status() != WL_CONNECTED ) {
     delay ( 500 ); Serial.print ( "." );
   }
-  
-  // WiFi connexion is OK
-  Serial.println ( "" ); 
+
+  // WiFi connection is OK
+  Serial.println ( "" );
   Serial.print ( "Connected to " ); Serial.println ( ssid );
   Serial.print ( "IP address: " ); Serial.println ( WiFi.localIP() );
-  
-  // link to the function that manage launch page 
+
+  // link to the function that manage launch page
   server.on ( "/", handleRoot );
   server.begin();
-  Serial.println ( "HTTP server started" ); 
+  Serial.println ( "HTTP server started" );
 }
- 
+
 void loop() {
   server.handleClient();
   delay(1000);
